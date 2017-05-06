4078d4103e0076e3e8eef10907fa8516db2df554
Anwesh43 <anweshthecool0@gmail.com>
2017-05-06 13:43:24 +0530

    Adding RevealImageList for containing the list

--- revealcoloredimagelist/src/main/java/com/anwesome/ui/revealcoloredimagelist/RevealImageList.java
+++ revealcoloredimagelist/src/main/java/com/anwesome/ui/revealcoloredimagelist/RevealImageList.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.revealcoloredimagelist;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.view.ViewGroup;
 import android.widget.ScrollView;
 
 /**
@@ -10,17 +11,22 @@ import android.widget.ScrollView;
 public class RevealImageList {
     private Activity activity;
     private ScrollView scrollView;
+    private RevealColoredImageList revealColoredImageList;
     private boolean isShown = false;
     public RevealImageList(Activity activity) {
         this.activity = activity;
+        revealColoredImageList = new RevealColoredImageList(activity);
+        scrollView = new ScrollView(activity);
+        scrollView.addView(revealColoredImageList,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
     public void addImageAndColor(Bitmap bitmap,int color) {
         if(!isShown) {
-
+            revealColoredImageList.addImage(bitmap,color);
         }
     }
     public void show() {
         if(!isShown) {
+            activity.setContentView(scrollView);
             isShown = true;
         }
     }
